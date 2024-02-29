dbf1b6f94a313dfc29a8ddd1ddfe34e9ede27268
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2024-02-29 15:00:22 +1300

    Working instructions v2

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -8,16 +8,24 @@ def instructions():
     statement_generator("Instructions", "-")
 
     print('''
-- Enter an integer that is between 1 and 200 (inclusive) to output the factors of that integer
-- If the integer is 1, it will tell you that 1 is unity as it only has one factor
-- If the integer is a prime number, it will say so and only have two factors
-- If the integer is a perfect square, it will say so and there will be an odd number of factors 
+To use this program, simply enter an integer between 
+1 and 200 (inclusive). The program will show the factors of 
+your chosen integer.
+
+It will also tell you if your chosen number ...
+- is unity (1) as it only has one factor
+- is a prime number (i.e. it has two factors)
+- is a perfect square
+
+To exit the program, please type 'xxx' 
     ''')
 
 
 # Main routine goes here
 
 # Display instructions if required
+statement_generator("The Ultimate Factor Finder", "*")
+
 want_instructions = input("Press <enter> to read the instructions or any key to continue")
 
 if want_instructions == "":
@@ -25,6 +33,3 @@ if want_instructions == "":
 
 
 print("program continues")
-
-
-
